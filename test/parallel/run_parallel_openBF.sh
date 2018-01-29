1d11d3fabdf08a4f87d528dc9fbe1ace7c75edff
ale <alessandro_melis@rocketmail.com>
2018-01-29 12:15:08 +0000

    Add p.sh

---
+++ test/parallel/run_parallel_openBF.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp ~/.julia/v0.6/openBF/main.jl ./main.jl && ~/julia0.6/bin/julia main.jl bifurcation
--- test/parallel/set.py
+++ test/parallel/set.py
@@ -3,3 +3,4 @@ import os
 for i in range(1, 25):
     os.mkdir(str(i))
     os.system("cp bifurcation* {0}/".format(i))
+    os.system("cp run_parallel_openBF.sh {0}/".format(i))
