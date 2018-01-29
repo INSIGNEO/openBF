1547cc737a169ebb0acadda9c979150f3e511fb9
ale <alessandro_melis@rocketmail.com>
2018-01-29 09:00:46 +0000

    Fix installation script

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,10 +4,7 @@ tar -xzvf julia-0.6.0-linux-x86_64.tar.gz
 mv julia-903644385b ~/julia0.6
 rm julia-0.6.0-linux-x86_64.tar.gz
 echo "alias julia='~/julia0.6/bin/julia'" >> ~/.bashrc
-source ~/.bashrc
 
-git clone https://github.com/INSIGNEO/openBF
-cd openBF
 mkdir -p ~/.julia/v0.6/openBF
 cp -r src ~/.julia/v0.6/openBF/
 cp main.jl ~/.julia/v0.6/openBF/
@@ -16,3 +13,4 @@ mkdir -p ~/.julia/v0.6/BTypes/src
 cp src/BTypes.jl ~/.julia/v0.6/BTypes/src/
 
 echo "alias openBF='cp ~/.julia/v0.6/openBF/main.jl ./main.jl && julia main.jl $1'" >> ~/.bashrc
+source ~/.bashrc
