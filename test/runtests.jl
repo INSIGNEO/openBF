d00c98ac1422151e7214d1cec5040251f0ef81b3
Alessandro Melis <alessandro_melis@rocketmail.com>
2018-08-10 11:33:56 +0100

    find() -> findall()

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,17 +6,17 @@ using openBF
 @testset "openBF.jl" begin
 
     #unit tests
-    # println("Test initialise.jl functions")
-    # include("test_initialise.jl")
+    println("Test initialise.jl functions")
+    include("test_initialise.jl")
 
-    # println("Test boundary_conditions.jl functions")
-    # include("test_boundary_conditions.jl")
+    println("Test boundary_conditions.jl functions")
+    include("test_boundary_conditions.jl")
 
-    # println("Test solver.jl functions")
+    println("Test solver.jl functions")
     # include("test_solver.jl")
 
     #integration tests
-    # include("test_single-artery.jl")
+    include("test_single-artery.jl")
     include("test_conjunction.jl")
     include("test_bifurcation.jl")
     include("test_aspirator.jl")
