bc9d21fb3a1ce1f1cd17c3d05ab122d98f7d2329
Alessandro Melis <alessandro_melis@rocketmail.com>
2018-08-10 10:51:08 +0100

    raw escape sequence

--- test/test_single-artery.jl
+++ test/test_single-artery.jl
@@ -3,11 +3,11 @@ cd(test_folder)
 
 openBF.runSimulation("$test_folder.yml", verbose=true, out_files=false)
 
-p = readdlm("$test_folder\_results/A1_P.last")
+p = readdlm(raw"$test_folder_results/A1_P.last")
 
 @test isapprox(minimum(p[:,end]), 9522.0, atol=1)
 @test isapprox(mean(p[:,end]), 12712.0, atol=1)
 @test isapprox(maximum(p[:,end]), 16772.0, atol=1)
 
-rm("$test_folder\_results", recursive=true)
+rm(raw"$test_folder_results", recursive=true)
 cd("..")
