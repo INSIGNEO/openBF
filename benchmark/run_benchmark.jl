f0fad47a1f6c9a3e3a62e77904e030082be67608
alessandro <alessandro_melis@rocketmail.com>
2019-11-30 12:52:04 +0000

    jbm

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -3,4 +3,4 @@ bmr = benchmarkpkg("openBF")
 export_markdown("bm_results.md", bmr)
 
 jbm = judge("openBF", "9cfab60ff269e43a5ed752c6ef3d6fdaf132efa1")
-export_markdown("bm_comparison.md", bmr)
+export_markdown("bm_comparison.md", jbm)
