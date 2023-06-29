4357b9b869f9adcf9884b01f5487c11d153fb058
alemelis <alessandro_melis@rocketmail.com>
2023-06-29 16:07:20 +0100

    viz

--- dashboard/pages/01_network_builder.py
+++ dashboard/pages/01_network_builder.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import yaml
+from streamlit_agraph import agraph, Node, Edge, Config
 from openbf_dashboard.blood import get_blood_props
 from openbf_dashboard.solver import get_solver_props
 from openbf_dashboard.vessel import get_vessel
+
 
 def parse_project_name(s: str) -> str:
     if s:
@@ -32,14 +35,65 @@ with c1:
     with st.expander("Solver"):
         y["solver"] = get_solver_props()
 
-    with st.expander("Network"):
-        st.text("network definition")
-        v = get_vessel()
+    st.divider()
+    y["network"] = []
+    vessels = st.number_input(
+        "number of vessels",
+        min_value=1,
+        step=1,
+    )
+    for i in range(vessels):
+        v = get_vessel(i)
         if v:
-            y["network"] = [v]
+            y["network"].append(v)
+
 
 with c2:
-    st.text("viz")
+    st.download_button(
+        "Download .yaml",
+        data=yaml.dump(
+            y,
+            sort_keys=False,
+            default_flow_style=False,
+        ),
+        file_name=f"{y['project_name']}.yaml",
+    )
+
+    nodes = []
+    edges = []
+    seen_nodes = []
+    for v in y["network"]:
+        if v["sn"] not in seen_nodes:
+            nodes.append(
+                Node(
+                    id=v["sn"],
+                    label=str(v["sn"]),
+                    size=10 if "inlet" in v else 5,
+                )
+            )
+            seen_nodes.append(v["sn"])
+
+        if v["tn"] not in seen_nodes:
+            nodes.append(
+                Node(
+                    id=v["tn"],
+                    label=str(v["tn"]),
+                    size=1 if "outlet" in v else 5,
+                )
+            )
+            seen_nodes.append(v["tn"])
+
+        edges.append(Edge(source=v["sn"], label=v["label"], target=v["tn"]))
+
+    config = Config(
+        width=750,
+        height=950,
+        directed=True,
+        hierarchical=False,
+        physics=True,
+    )
+
+    agraph(nodes=nodes, edges=edges, config=config)
 
 with c1:
     st.divider()
