8813b304ce31df2e0e949a3505133ffbdb684fb0
alemelis <alessandro_melis@rocketmail.com>
2024-03-21 14:23:02 +0000

    restore graph

--- dashboard/pages/01_Network_Building_Tool.py
+++ dashboard/pages/01_Network_Building_Tool.py
@@ -12,85 +12,85 @@ st.set_page_config(
     layout="wide",
 )
 
-# c1, c2 = st.columns([1, 1])
+c1, c2 = st.columns([1, 1])
 
-# with c1:
-y = {}
+with c1:
+    y = {}
 
-with st.expander("Input/Output"):
-    y |= get_io()
+    with st.expander("Input/Output"):
+        y |= get_io()
 
-with st.expander("Blood"):
-    y["blood"] = get_blood_props()
+    with st.expander("Blood"):
+        y["blood"] = get_blood_props()
 
-with st.expander("Solver"):
-    y["solver"] = get_solver_props()
-
-
-
-st.divider()
-y["network"] = []
-vessels = st.number_input(
-    "number of vessels",
-    min_value=1,
-    step=1,
-)
-for i in range(vessels):
-    v = get_vessel(i)
-    if v:
-        y["network"].append(v)
+    with st.expander("Solver"):
+        y["solver"] = get_solver_props()
 
 
-# with c2:
-#     st.download_button(
-#         "Download .yaml",
-#         data=yaml.dump(
-#             y,
-#             sort_keys=False,
-#             default_flow_style=False,
-#         ),
-#         file_name=f"{y['project_name']}.yaml",
-#     )
 
-#     with st.expander("Graph"):
-#         nodes = []
-#         edges = []
-#         seen_nodes = []
-#         for v in y["network"]:
-#             if v["sn"] not in seen_nodes:
-#                 nodes.append(
-#                     Node(
-#                         id=v["sn"],
-#                         label=str(v["sn"]),
-#                         size=10 if "inlet" in v else 5,
-#                     )
-#                 )
-#                 seen_nodes.append(v["sn"])
+    st.divider()
+    y["network"] = []
+    vessels = st.number_input(
+        "number of vessels",
+        min_value=1,
+        step=1,
+    )
+    for i in range(vessels):
+        v = get_vessel(i)
+        if v:
+            y["network"].append(v)
 
-#             if v["tn"] not in seen_nodes:
-#                 nodes.append(
-#                     Node(
-#                         id=v["tn"],
-#                         label=str(v["tn"]),
-#                         size=1 if "outlet" in v else 5,
-#                     )
-#                 )
-#                 seen_nodes.append(v["tn"])
 
-#             edges.append(Edge(source=v["sn"], label=v["label"], target=v["tn"]))
+with c2:
+    st.download_button(
+        "Download .yaml",
+        data=yaml.dump(
+            y,
+            sort_keys=False,
+            default_flow_style=False,
+        ),
+        file_name=f"{y['project_name']}.yaml",
+    )
 
-#         config = Config(
-#             width=750,
-#             height=950,
-#             directed=True,
-#             hierarchical=False,
-#             physics=True,
-#         )
+    with st.expander("Graph"):
+        nodes = []
+        edges = []
+        seen_nodes = []
+        for v in y["network"]:
+            if v["sn"] not in seen_nodes:
+                nodes.append(
+                    Node(
+                        id=v["sn"],
+                        label=str(v["sn"]),
+                        size=10 if "inlet" in v else 5,
+                    )
+                )
+                seen_nodes.append(v["sn"])
 
-#         agraph(nodes=nodes, edges=edges, config=config)
+            if v["tn"] not in seen_nodes:
+                nodes.append(
+                    Node(
+                        id=v["tn"],
+                        label=str(v["tn"]),
+                        size=1 if "outlet" in v else 5,
+                    )
+                )
+                seen_nodes.append(v["tn"])
 
-# with c1:
-st.divider()
+            edges.append(Edge(source=v["sn"], label=v["label"], target=v["tn"]))
 
-if y:
-    st.json(y)
+        config = Config(
+            width=750,
+            height=950,
+            directed=True,
+            hierarchical=False,
+            physics=True,
+        )
+
+        agraph(nodes=nodes, edges=edges, config=config)
+
+with c1:
+    st.divider()
+
+    if y:
+        st.json(y)
