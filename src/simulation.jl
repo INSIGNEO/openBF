c63962c5e57b7a4f269d97b1b87e5e03ab8c49e8
alemelis <alemelis@users.noreply.github.com>
2024-03-27 13:28:49 +0000

    Explain output files format (#61)
    * add out format docs
    
    * fix preamble

--- src/simulation.jl
+++ src/simulation.jl
@@ -77,7 +77,9 @@ function preamble(yaml_config, verbose)
     isdir(results_dir) && rm(results_dir, recursive = true)
     ~isdir(results_dir) && mkdir(results_dir)
     cp(yaml_config, joinpath(results_dir, yaml_config), force = true)
-    cp(config["inlet_file"], joinpath(results_dir, config["inlet_file"]), force = true)
+
+    inlet_file = get_inlet_file(config)
+    cp(inlet_file, joinpath(results_dir, inlet_file), force = true)
     cd(results_dir)
 
     config, temp_save
@@ -87,7 +89,6 @@ function run_simulation(
     yaml_config::String;
     verbose::Bool = true,
     out_files::Bool = false,
-    conv_ceil::Bool = false,
 )
     initial_dir = pwd()
     config, temp_save = preamble(yaml_config, verbose)
